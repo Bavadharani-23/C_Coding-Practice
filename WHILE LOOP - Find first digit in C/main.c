a96a05e3b7baddbab5c63e5f1dc8c0fca2f74bb3
Bavadharini-23 <bavadharanir191997@gmail.com>
2026-01-16 11:57:50 +0530

    Add find the first digit in C using WHILE LOOP

---
+++ WHILE LOOP - Find first digit in C/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    int original = n;
+    while(n>=10)
+    {
+        n=n/10;
+    }
+    printf("%d is the first digit of %d",n,original);
+    return 0;
+}
